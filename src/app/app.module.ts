12523ab0646f50fb0fb94c15587841261d30e371
Arif Ali <meer.aafay@gmail.com>
2026-03-11 01:53:10 +0500

    feature/ add-header-component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiAlertModule, TuiButtonModule, TuiDialogModule, TuiNotificationModule, TuiRootModule } from '@taiga-ui/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiNotificationModule,
+    TuiButtonModule,
+    TuiAlertModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
